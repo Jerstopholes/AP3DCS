8e6d750ce668de4fa7594fe9b20357ba584d85bc
Jeremy <jerstopholes@vivaldi.net>
2021-09-05 17:52:42 -0500

    Added First-Person Functionality
    Added the ability to change into first-person mode if desired!

--- camera/CameraInput.gd
+++ camera/CameraInput.gd
@@ -4,15 +4,16 @@ extends Node
 # This script houses the Input settings for the Camera. 
 
 # These exported strings can be used to overide the default input action names.
-export (String) var LookLeft = "*left*"
-export (String) var LookRight = "*right*"
-export (String) var LookUp = "*up*"
-export (String) var LookDown = "*down*"
-export (String) var ZoomKey = "*zoom_keyboard*"
-export (String) var ZoomButton = "*zoom_gamepad*"
-export (String) var CenterCameraKey = "*center_camera_keyboard*"
-export (String) var CenterCameraButton = "*center_camera_button*"
+export (String) var look_left = "*left*"
+export (String) var look_right = "*right*"
+export (String) var look_up = "*up*"
+export (String) var look_down = "*down*"
+export (String) var zoom_key = "*zoom_keyboard*"
+export (String) var zoom_button = "*zoom_gamepad*"
+export (String) var center_camera_key = "*center_camera_keyboard*"
+export (String) var center_camera_button = "*center_camera_button*"
 
+# Get the camera
 onready var _camera = get_parent()
 
 # Called when the node enters the scene tree for the first time.
@@ -20,7 +21,7 @@ func _ready():
 	
 	# Check if we have selected to generate inputs automatically, and if we have,
 	# then generate the inputs!
-	if _camera.InputsToUse == _camera.Inputs.GENERATE_FOR_ME:
+	if _camera.inputs_to_use == _camera.inputs.GENERATE_FOR_ME:
 		_create_input_mappings()
 
 
@@ -30,50 +31,63 @@ func _create_input_mappings():
 	
 	# first check for horizontal rotation, add it if it doesn't exist
 	# left
-	if !InputMap.has_action("*left*"):
-		InputMap.add_action("*left*", 0.5)
+	if !InputMap.has_action(look_left):
+		InputMap.add_action(look_left, 0.5)
 		var event = InputEventJoypadMotion.new()
 		event.axis = JOY_AXIS_2
 		event.axis_value = -1.0
-		InputMap.action_add_event("*left*", event)
+		InputMap.action_add_event(look_left, event)
 	
 	# right
-	if !InputMap.has_action("*right*"):
-		InputMap.add_action("*right*", 0.5)
+	if !InputMap.has_action(look_right):
+		InputMap.add_action(look_right, 0.5)
 		var event = InputEventJoypadMotion.new()
 		event.axis = JOY_AXIS_2
 		event.axis_value = 1.0
-		InputMap.action_add_event("*right*", event)
+		InputMap.action_add_event(look_right, event)
 	
 	# now check for the vertical rotation, if it doesn't exist add it
 	# up
-	if !InputMap.has_action("*up*"):
-		InputMap.add_action("*up*", 0.5)
+	if !InputMap.has_action(look_up):
+		InputMap.add_action(look_up, 0.5)
 		var event = InputEventJoypadMotion.new()
 		event.axis = JOY_AXIS_3
 		event.axis_value = -1.0
-		InputMap.action_add_event("*up*", event)
+		InputMap.action_add_event(look_up, event)
 	
 	# down
-	if !InputMap.has_action("*down*"):
-		InputMap.add_action("*down*", 0.5)
+	if !InputMap.has_action(look_down):
+		InputMap.add_action(look_down, 0.5)
 		var event = InputEventJoypadMotion.new()
 		event.axis = JOY_AXIS_3
 		event.axis_value = 1.0
-		InputMap.action_add_event("*down*", event)
+		InputMap.action_add_event(look_down, event)
 	
 	
 	# now add zoom support
 	# controller
-	if !InputMap.has_action("*zoom_gamepad*"):
-		InputMap.add_action("*zoom_gamepad*", 0.5)
+	if !InputMap.has_action(zoom_button):
+		InputMap.add_action(zoom_button, 0.5)
 		var event = InputEventJoypadButton.new()
 		event.button_index = 10
-		InputMap.action_add_event("*zoom_gamepad*", event)
+		InputMap.action_add_event(zoom_button, event)
 	
 	# mouse
-	if !InputMap.has_action("*zoom_keyboard*"):
-		InputMap.add_action("*zoom_keyboard*", 0.5)
+	if !InputMap.has_action(zoom_key):
+		InputMap.add_action(zoom_key, 0.5)
 		var event = InputEventKey.new()
 		event.scancode = KEY_C
-		InputMap.action_add_event("*zoom_keyboard*", event)
+		InputMap.action_add_event(zoom_key, event)
+		
+	# Recentering camera
+	if !InputMap.has_action(center_camera_key):
+		InputMap.add_action(center_camera_key)
+		var event = InputEventKey.new()
+		event.scancode = KEY_V
+		InputMap.action_add_event(center_camera_key, event)
+		
+	if !InputMap.has_action(center_camera_button):
+		InputMap.add_action(center_camera_button)
+		var event = InputEventJoypadButton.new()
+		event.button_index = 9
+		InputMap.action_add_event(center_camera_button, event)
